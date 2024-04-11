662d163ee95dbb5896c5ad0cecc1b538a1fe95f2
不做超级小白 <12001216+XoX_escape@user.noreply.gitee.com>
2024-04-11 15:19:11 +0000

    v-bind实现动态面板
    Signed-off-by: 不做超级小白 <12001216+XoX_escape@user.noreply.gitee.com>

---
+++ v-bind动态面板/v-bind动态面板.js
@@ -0,0 +1,15 @@
+const tab = new Vue({
+    el: '#tab',
+    data: {
+        show_state: 1,
+        click_index: 0,
+        ul_list: [
+            {id: 1,title: "State1"},
+            {id: 2,title: "State2"},
+            {id: 3,title: "State3"}
+        ]
+    },
+    methods: {
+
+    }
+})
